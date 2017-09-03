2e9701df069eac824b0985f6416c55dcf8cde351
Shiva Poudel <dev.shivapoudel@gmail.com>
2017-09-03 13:30:43 +0545

    Make checkbox hidden so no message is displayed

--- includes/views/html-license-form.php
+++ includes/views/html-license-form.php
@@ -11,9 +11,10 @@ $license_row = sanitize_title( $this->plugin_slug . '_license_row' );
 $license_key = sanitize_title( $this->plugin_slug . '_license_key' );
 
 ?>
-<tr id="<?php echo esc_attr( $license_row ); ?>" class="active plugin-update-tr rp-license-key-row-tr">
-	<td class="plugin-update" colspan="3">
-		<div class="rp-license-key-row">
+<tr id="<?php echo esc_attr( $license_row ); ?>" class="plugin-update-tr rp-license-key-row-tr active">
+	<td colspan="3" class="plugin-update colspanchange">
+		<input type="checkbox" name="checked[]" value="1" checked="checked" style="display: none;">
+		<div class="rp-license-key-row inline">
 			<label for="<?php echo $license_key ?>"><?php _e( 'License:', 'user-registration' ); ?></label>
 			<input type="text" id="<?php echo $license_key; ?>" name="<?php echo esc_attr( $license_key ); ?>" placeholder="<?php echo esc_attr( 'XXXX-XXXX-XXXX-XXXX', 'restaurantpress' ); ?>" />
 			<span class="description"><?php _e( 'Enter your license key and hit return. A valid key is required for updates.' ); ?> <?php printf( 'Lost your key? <a href="%s">Retrieve it here</a>.', esc_url( 'https://wpeverest.com/lost-licence-key/' ) ); ?></span>
