7be881beefba896ee1d084ddcfd058ec7980c9f0
Shiva Poudel <dev.shivapoudel@gmail.com>
2017-09-04 00:40:30 +0545

    Fix

--- includes/class-rp-plugin-updater.php
+++ includes/class-rp-plugin-updater.php
@@ -72,7 +72,6 @@ class RP_Plugin_Updater {
 		add_filter( 'block_local_requests', '__return_false' );
 		add_action( 'admin_init', array( $this, 'admin_init' ) );
 
-		include_once( dirname( __FILE__ ) . '/admin/updater/class-rp-addon-updater.php' );
 		include_once( dirname( __FILE__ ) . '/admin/updater/class-rp-plugin-updater-api.php' );
 	}
 
